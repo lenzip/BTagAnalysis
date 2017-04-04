1ddb873eb736d8ad46d6969a9b13f0f9c33ad8d8
Piergiulio <piergiulio.lenzi@cern.ch>
2017-04-04 17:09:58 +0200

    added global script

---
+++ submitAll.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+echo "Submitting DATA...."
+for i in `ls lists | grep BTagMu`; do
+  bsub -q cmscaf1nd -o $PWD/logs/$i "submit.sh $i -d"
+done  
+echo "Submitting MC...."
+for i in `ls lists | grep MuEnriched`; do
+  bsub -q cmscaf1nd -o $PWD/logs/$i "submit.sh $i"
+done
+
